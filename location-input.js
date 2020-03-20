7349b79924bbafe9bd342741278b636ef998ca05
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-03-19 20:31:40 -0400

    Added event listener for DOMContentLoaded to render JSON data to index.html

--- location-input.js
+++ location-input.js
@@ -27,32 +27,63 @@
     - https://api.opencagedata.com/geocode/v1/json?q=Rua+Cafel%C3%A2ndia%2C+Carapicu%C3%ADba%2C+Brasil&key=YOUR-API-KEY&pretty=1
 
 ------------------------------------------------------------------
+
+    Current Issues:
+    - Figure out how we want to handle multiple returned results
+    - Rendering results to the screen
+        - Need a promise for this
+    - Return lat/long of current IP
+
+------------------------------------------------------------------
 */
 
-// Pulling in axios
-const axios = require('axios');
+
+document.addEventListener('DOMContentLoaded', function(){
+    let cityData = document.getElementById('mainPage').innerHTML = getCityData('Atlanta', 'Georgia', 'US');
+    return `
+        <div>${cityData}</div>
+    `
+})
+
+// Pulling in axios for console runs
+// const axios = require('axios');
 
 // Declare API key and url for query
 // *key will eventually need to be stored in safer place since this is a public repo*
 const apiKey = '69355e5ac9ff4d22abfe958f18f4dbaa';
 const url = `https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&pretty=1&q=`;
 
-// Function to call api given city and state
-// For now just defining city and state as static variables
-const city = 'Atlanta';
-const state = 'Georgia';
-const country = 'US';
+// Variable definitions from the html
+// let city = document.getElementById().value;
+// let state = document.getElementById().value;
 
+// For now just defining city and state as static variables
+/* const city = 'Atlanta';
+const state = 'Georgia';
+const country = 'US'; */
+
+// Function to call api given city and state
 getCityData = (city, state, country) => {
     console.log(`${url}${city}%2C+${state}%2C+${country}`);
-
+    let cityData;
     axios.get(`${url}${city}%2C+${state}%2C+${country}`)
         .then(function(response){
-            console.log(response.data);
+            console.log(response.data.results);
+            cityData = JSON.stringify(response.data.results);
         })
         .catch(function(error){
             console.error(error);
          })
+    return cityData;
 }
 
-getCityData(city, state, country);
+
+/* let p = new Promise((resolve, reject) => {
+    resolve(getCityData('Atlanta', 'Georgia', 'US'));
+})
+
+p.then(function (value) {
+    console.log('DONE: ', value);
+}).catch(function(error) {
+    console.log(error);
+}) */
