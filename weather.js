23500c508bf072447ad5729bbb53fbd04ecb425b
Michael Stallings <stall84@gmail.com>
2020-03-19 21:17:33 -0400

    got weather.js working with a console.log of the weatherbit api

--- weather.js
+++ weather.js
@@ -1,2 +1,10 @@
 
+const wxKey = 'c5a98bfa515d48ffa7aa8ce2da231083'
+const wxUrl = `https://api.weatherbit.io/v2.0/current?city=Atlanta,GA&key=${wxKey}`
+const weatherDiv = document.getElementById('weather');
+const localWeather = axios.get(wxUrl)
+    .then(function(response) {
+        console.log(response.data)
+        //weatherDiv.innerHTML = renderWX(data);
+    });
 
