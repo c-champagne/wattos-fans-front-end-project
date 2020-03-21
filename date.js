914e2614d4cc99dcc489dad801c9b5a315e83271
seid zerihun <seidoo99@gmial.com>
2020-03-21 12:57:05 -0400

    add latlong

--- date.js
+++ date.js
@@ -14,11 +14,23 @@ function astronomyData(data) {
     return `
     Your current location is set to: ${data.location.city}, ${data.location.state_prov}.  Today's sunrise is at ${data.sunrise} and sunset is at ${data.sunset}.
     `
-/*     return ` Moving output into NavBar.  Replaced with code above.
-        <div>city: ${data.location.city}</div>
-        <div>date: ${data.date}</div>
-        <div>sunrise: ${data.sunrise}</div>
-        <div>sunset: ${data.sunset}</div>
-        
-        ` */
+        /*     return ` Moving output into NavBar.  Replaced with code above.
+                <div>city: ${data.location.city}</div>
+                <div>date: ${data.date}</div>
+                <div>sunrise: ${data.sunrise}</div>
+                <div>sunset: ${data.sunset}</div>
+                
+                ` */
 }
+
+function Coordinate() {
+    let latLon;
+    var data = axios.get('https://api.ipgeolocation.io/astronomy?apiKey=14254d6a171646dc887404abf24bdbcc')
+        .then(function(response) {
+            coordinateDatalat = response.data.location.latitude;
+            cordinateDataLan = response.data.location.longitude;
+            latLon = coordinateDatalat + "," + cordinateDataLan;
+            console.log(latLon)
+        })
+}
+Coordinate()
