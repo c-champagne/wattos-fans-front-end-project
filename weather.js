21b61bc4d247692faf8c8fe86ea27f5856030e40
Michael Stallings <stall84@gmail.com>
2020-03-21 09:58:33 -0400

    able to access the individual weather objects now by adding two data paths (response.data.data[0])

--- weather.js
+++ weather.js
@@ -2,9 +2,19 @@
 const wxKey = 'c5a98bfa515d48ffa7aa8ce2da231083'
 const wxUrl = `https://api.weatherbit.io/v2.0/current?city=Atlanta,GA&key=${wxKey}`
 const weatherDiv = document.getElementById('weather');
+var cloudCover;
 const localWeather = axios.get(wxUrl)
     .then(function(response) {
-        console.log(response.data)
+        cloudCover = response.data.data[0].clouds;
+        //console.log(response);
+        //console.log(response.data.data[0].clouds);
+        //console.log(response.data.data[0].weather.description);
         //weatherDiv.innerHTML = renderWX(data);
+        
     });
+
+    console.log(localWeather);
+
+
+    
 
