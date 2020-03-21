d62766245366bf015e4bb737f24dad855b401fdd
Michael Stallings <stall84@gmail.com>
2020-03-21 13:24:58 -0400

    made numerous changes including adding/wiring Coordinates function from Seid that takes in forecaster function to currently give local weather/astro data returned. so both api's are communicating with each other now. still need to delete extra js files (weather,date,etc)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // purpose check astronomy API and render 
 // putting axios link to index.html
 // render the data to see on the UI
-var date = document.getElementById('date')
-var data = axios.get('https://api.ipgeolocation.io/astronomy?apiKey=14254d6a171646dc887404abf24bdbcc')
-    .then(function(response) {
-        date.innerHTML = astronomyData(response.data)
-    })
+//var date = document.getElementById('date')
+//var data = axios.get('https://api.ipgeolocation.io/astronomy?apiKey=14254d6a171646dc887404abf24bdbcc')
+    //.then(function(response) {
+        //date.innerHTML = astronomyData(response.data)
+    //})
 
 /*
     Onscreen:
@@ -16,12 +16,12 @@ var data = axios.get('https://api.ipgeolocation.io/astronomy?apiKey=14254d6a1716
         - moonrise/set
         - sunrise/set
 */
-function astronomyData(data) {
+//function astronomyData(data) {
 
 
-    return `
-    Your current location is set to: ${data.location.city}, ${data.location.state_prov}.  Today's sunrise is at ${data.sunrise} and sunset is at ${data.sunset}.
-    `
+    //return `
+    //Your current location is set to: ${data.location.city}, ${data.location.state_prov}.  Today's sunrise is at ${data.sunrise} and sunset is at ${data.sunset}.
+    //`
 /*     return ` Moving output into NavBar.  Replaced with code above.
         <div>city: ${data.location.city}</div>
         <div>date: ${data.date}</div>
@@ -29,7 +29,7 @@ function astronomyData(data) {
         <div>sunset: ${data.sunset}</div>
         
         ` */
-}
+
 
 
 
@@ -41,24 +41,43 @@ function astronomyData(data) {
     Variables:
         - cloud coverage
 */
+
+function Coordinate() {
+    let latLon;
+    axios.get('https://api.ipgeolocation.io/astronomy?apiKey=14254d6a171646dc887404abf24bdbcc')
+        .then(function(response) {
+            coordinateDatalat = response.data.location.latitude;
+            cordinateDataLan = response.data.location.longitude;
+            latLon = coordinateDatalat + "," + cordinateDataLan;
+            
+           
+                console.log(latLon)
+                forecaster(latLon);
+
+
+        })
+}
+
 function forecaster(latLon) {
 
 
 
-const wxKey = '0eab6e3837ad474491b152802202103';
-const wxURL = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${wxKey}&q=${latLon}&format=json`;
+    const wxKey = '0eab6e3837ad474491b152802202103';
+    const wxURL = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${wxKey}&q=${latLon}&format=json`;
+    console.log(wxURL);
+    
+    axios.get(wxURL)
+        .then(function(response) {
+    
+            //let cloudCvr = parseInt(response.data.data.current_condition[0].cloudcover);
+    
+            //console.log(cloudCvr);
+            console.log(response);
+        })
+    
+    }
 
-axios.get(wxURL)
-    .then(function(response) {
+Coordinate()
 
-        //let cloudCvr = parseInt(response.data.data.current_condition[0].cloudcover);
-
-        //console.log(cloudCvr);
-        console.log(response);
-    })
-
-}
-
-forecaster();
 
 // Compare cloud coverage to moonrise
