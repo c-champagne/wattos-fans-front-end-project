0756850da303d787f52a969a8d2e2a444ae9b89a
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-03-26 21:14:01 -0400

    Testing script.js

--- script.js
+++ script.js
@@ -187,42 +187,6 @@ function forecaster(latLon) {
                 ` */
             } 
             
-            if (tomCloudCvr > 0 && tomCloudCvr < 26 && tomMoonIllum > 0 && tomMoonIllum < 26) {
-
-                tomorrowDiv.innerHTML = `
-                <p> GOOD VIEWING CONDITIONS</p>
-                `
-                /* cloudString.innerHTML = `
-                ${cloudStringVal}
-                `
-                tempHolder.innerHTML = `
-                ${currTemp}F
-                `
-                var resultBG = document.getElementById("mainPage"); /* <-- Cassie's results img render
-                resultBG.style.backgroundImage="url('images/midView.jpg')";
-
-                /* cloudImg.innerHTML = ` <--Michael's original results img render--
-                <img style="height:200px;width:280px;" src="images/midView.jpg">
-                ` */
-            } else {
-
-                tomorrowDiv.innerHTML = `
-                <p> BAD VIEWING CONDITIONS</p>
-                <img src="images/badViewMini.png" style="width:auto;height:100px">
-                `
-                /* cloudString.innerHTML = `
-                ${cloudStringVal}
-                `
-                tempHolder.innerHTML = `
-                ${currTemp}F
-                `
-                var resultBG = document.getElementById("mainPage"); /* <-- Cassie's results img render
-                resultBG.style.backgroundImage="url('images/badView.jpg')";
-
-                /* cloudImg.innerHTML = ` <--Michael's original results img render--
-                <img style="height:200px;width:280px;" src="images/badView.jpg">
-                ` */
-            };
             // Attempt to make a for loop function
 
             for (let i = 1; i <= 2; i++) {
